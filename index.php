2bfa51a6a4d107bb8ff60566a68dab82d34e5f79
MJSato <soccer.1gk.hiroki@gmail.com>
2019-07-02 12:08:56 +0900

    l

--- index.php
+++ index.php
@@ -552,30 +552,30 @@
 			<div class="row blogs_container">
 				<div class="col-lg-4 blog_item_col">
 					<div class="blog_item">
-						<div class="blog_background" style="background-image:url(images/blog_1.jpg)"></div>
+						<div class="blog_background" style="background-image:url(images/chair2/54step.jpg)"></div>
 						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-							<h4 class="blog_title">Here are the trends I see coming this fall</h4>
-							<span class="blog_meta">by admin | dec 01, 2017</span>
+							<h4 class="blog_title">おしゃれな椅子の作り方</h4>
+							<span class="blog_meta">by admin | dec 01, 2019</span>
 							<a class="blog_more" href="#">Read more</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-4 blog_item_col">
 					<div class="blog_item">
-						<div class="blog_background" style="background-image:url(images/blog_2.jpg)"></div>
+						<div class="blog_background" style="background-image:url(images/shelf5/13-5.jpg)"></div>
 						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-							<h4 class="blog_title">Here are the trends I see coming this fall</h4>
-							<span class="blog_meta">by admin | dec 01, 2017</span>
+							<h4 class="blog_title">収納棚の作り方</h4>
+							<span class="blog_meta">by admin | dec 01, 2019</span>
 							<a class="blog_more" href="#">Read more</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-4 blog_item_col">
 					<div class="blog_item">
-						<div class="blog_background" style="background-image:url(images/blog_3.jpg)"></div>
+						<div class="blog_background" style="background-image:url(images/goods2/cover_4.jpg)"></div>
 						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-							<h4 class="blog_title">Here are the trends I see coming this fall</h4>
-							<span class="blog_meta">by admin | dec 01, 2017</span>
+							<h4 class="blog_title">小物入れ作り方</h4>
+							<span class="blog_meta">by admin | dec 01, 2019</span>
 							<a class="blog_more" href="#">Read more</a>
 						</div>
 					</div>
