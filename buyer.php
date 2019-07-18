ba12d71d1cd26f3f7b4699880d3e637e47dcdbeb
aita1017 <51937181+aita1017@users.noreply.github.com>
2019-07-18 11:39:04 +0900

    a

---
+++ buyer.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<meta charset="utf-8" />
+
+<title>buyer</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Colo Shop Template">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+<link rel="stylesheet" type="text/css" href="styles/buy.css">
+</head>
+
+<body>
+
+<div class="super_container">
+<!-- Header -->
+<header class="header trans_300">
+
+		<div class="top_nav">
+		</div>
+
+		<!-- Main Navigation -->
+
+		<div class="main_nav_container">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-right">
+						<div class="logo_container">
+							<a href="index.html"><img src="images/logo1.jpg" alt="" width="100" height="100"></a>
+						</div>
+						<nav class="navbar">
+							<ul class="navbar_menu">
+								<li><a href="#">product</a></li>
+								<li><a href="#">post</a></li>
+								<li><a href="#">new post</a></li>
+
+								<li><a href="contact.html">contact</a></li>
+							</ul>
+
+							<ul class="navbar_user">
+								<li><a href="#"><i class="fa fa-bell" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a>
+								<ul class="menu__second-level">
+										<li><a href="login.html"><i class="fa fa-sign-in" aria-hidden="true"></i>login</a></li>
+										<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>sign up</a></li>
+									</ul>
+							</li>
+								<li class="checkout">
+									<a href="#">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+									</a>
+								</li>
+							</ul>
+
+							<div class="hamburger_container">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</header>
+<br><br><br><br><br><br><br><br>
+<caption><center>ご注文内容</center></caption><br>
+<body><center>
+<table border width="90%">
+<thead>
+	<tr align="center">
+		<th>商品名</th><th>個数</th><th>配送</th><th>単価</th></tr>
+</thead>
+<tbody>
+   <tr align="center" >
+	   <td><img src="./images/0000-s.jpg"><br><a href="#">sevenstar</a></td><td>1個</td><td>送料込み</td><td>500円</td></tr>
+</tbody>
+</table>
+</center></body>
+<br>
+<body><center>
+<table size="3"width="90%"cellpadding="20px">
+<tr align="center" height="40px"><th>支払方法</th><td>コンビニ払い<br><small>手数料 : ＋100円</small></td></tr>
+<tr align="center" height="80px"><th>配送</th><td>日付指定:2019/7/26</td></tr><br>
+<tr align="center" height="80px"><th>支払金額</th><td>600円</td></tr><br>
+<tr align="center" height="80px"><th>支払い状況</th><td>未払い</td></tr>
+</table>
+</center></body>
+<br>
+
+<caption><center>配送先</center></caption><br>
+<body><center>
+<table width="70%"cellpadding="20px">
+<thead>
+	<tr align="center">
+		<th>名前</th><th>郵便番号</th><th>住所</th></tr>
+</thead>
+<tbody>
+   <tr align="center" >
+	   <td>wiz 太郎</td><td>〒987-6543</td><td>福島県郡山方八町</td></tr>
+</tbody>
+</table>
+</center></body>
+<br>
+
+
+</p>
+<form action="xxx.php" method="post" >
+	
+</form>
+<br><br>
+
+
+<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
+
+						<ul class="footer_nav">
+<li><a href="contact.html">お問い合わせ</a></li>
+							<li><a href="privacy.html">プライバシーポリシー</a></li>
+							<li><a href="kiyaku.html">利用規約</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
+						<ul>
+							<li><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="https://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="https://instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="footer_nav_container">
+						<div class="cr">©2019 TeamF imagecraft</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
