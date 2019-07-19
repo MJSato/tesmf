9559b6fb7f75d48a790f8480dcb2f8df10e98641
MJSato <soccer.1gk.hiroki@gmail.com>
2019-07-19 16:05:31 +0900

    j

--- categories.php
+++ categories.php
@@ -1,331 +1,184 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>IMAGE CRAFT</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Colo Shop Template">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
-<link rel="stylesheet" type="text/css" href="plugins/jquery-ui-1.12.1.custom/jquery-ui.css">
-<link rel="stylesheet" type="text/css" href="styles/categories_styles.css">
-<link rel="stylesheet" type="text/css" href="styles/categories_responsive.css">
-<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<?php
 
-</head>
+//ライブラリをインクルード
+require_once("inc_base.php");
+require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
+require_once("inc_smarty.php");
+//以下はセッション管理用のインクルード
+require_once($CMS_COMMON_INCLUDE_DIR . "auth_adm.php");
 
-<body>
+$show_mode = '';
+$ERR_STR = '';
 
-<div class="super_container">
+//ページの設定
+//デフォルトは1
+$page = 1;
+//もしページが指定されていたら
+if(isset($_GET['page']) 
+    //なおかつ、数字だったら
+    && cutil::is_number($_GET['page'])
+    //なおかつ、0より大きかったら
+    && $_GET['page'] > 0){
+    //パラメータを設定
+    $page = $_GET['page'];
+}
 
-	<!-- Header -->
+//1ページのリミット
+$limit = 20;
+$rows = array();
 
-	<header class="header trans_300">
+if(is_func_active()){
+    if(param_chk()){
+        switch($_POST['func']){
+            case "del":
+                $show_mode = 'del';
+                //削除操作
+                deljob();
+                //リダイレクトするページの計算
+                $re_page = $page;
+                $obj = new cUSERS();
+                $allcount = $obj->get_all_count(false);
+                $last_page = (int)($allcount / $limit);
+                if($allcount % $limit){
+                    $last_page++;
+                }
+                if($re_page > $last_page){
+                    $re_page = $last_page;
+                }
+                //再読み込みのためにリダイレクト
+                cutil::redirect_exit($_SERVER['PHP_SELF'] 
+                . '?page=' . $re_page);
+            break;
+            default:
+            break;
+        }
+    }
+}
+$show_mode = 'edit';
+//データの読み込み
+readdata();
 
-		<!-- Top Navigation -->
+/////////////////////////////////////////////////////////////////
+/// 関数ブロック
+/////////////////////////////////////////////////////////////////
 
-		<div class="top_nav">
-		
-		</div>
-
-		<!-- Main Navigation -->
-
-		<div class="main_nav_container">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 text-right">
-						<div class="logo_container">
-						<a href="index.php"><img src="images/logo1.jpg" alt="" width="100" height="100"></a>
-						</div>
-						<nav class="navbar">
-							<ul class="navbar_menu">
-							<li><a href="categories.php">product</a></li>
-								<li><a href="#">post</a></li>
-								<li><a href="#">new post</a></li>
-								<li><a href="contact.php">contact</a></li>
-							</ul>
-
-							<ul class="navbar_user">
-								<li><a href="#"><i class="fa fa-bell" aria-hidden="true"></i></a></li>
-								
-								<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a>
-										<li><a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
-										<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i></a></li>
-							</li>
-								<li class="checkout">
-									<a href="#">
-										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
-									</a>
-								</li>
-							</ul>
-
-							<div class="hamburger_container">
-								<i class="fa fa-bars" aria-hidden="true"></i>
-							</div>
-						</nav>
-					</div>
-				</div>
-			</div>
-		</div>
-
-	</header>
-
-	<div class="fs_menu_overlay"></div>
-
-	<!-- Hamburger Menu -->
-
-	<div class="hamburger_menu">
-		<div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
-		<div class="hamburger_menu_content text-right">
-			<ul class="menu_top_nav">
-				<li class="menu_item has-children">
-					<a href="#">
-						usd
-						<i class="fa fa-angle-down"></i>
-					</a>
-					<ul class="menu_selection">
-						<li><a href="#">cad</a></li>
-						<li><a href="#">aud</a></li>
-						<li><a href="#">eur</a></li>
-						<li><a href="#">gbp</a></li>
-					</ul>
-				</li>
-				<li class="menu_item has-children">
-					<a href="#">
-						English
-						<i class="fa fa-angle-down"></i>
-					</a>
-					<ul class="menu_selection">
-						<li><a href="#">French</a></li>
-						<li><a href="#">Italian</a></li>
-						<li><a href="#">German</a></li>
-						<li><a href="#">Spanish</a></li>
-					</ul>
-				</li>
-				<li class="menu_item has-children">
-					<a href="#">
-						My Account
-						<i class="fa fa-angle-down"></i>
-					</a>
-					<ul class="menu_selection">
-						<li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
-						<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
-					</ul>
-				</li>
-				<li class="menu_item"><a href="#">home</a></li>
-				<li class="menu_item"><a href="#">shop</a></li>
-				<li class="menu_item"><a href="#">promotion</a></li>
-				<li class="menu_item"><a href="#">pages</a></li>
-				<li class="menu_item"><a href="#">blog</a></li>
-				<li class="menu_item"><a href="#">contact</a></li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="container product_section_container">
-		<div class="row">
-			<div class="col product_section clearfix">
-
-				<!-- Breadcrumbs -->
-
-				<div class="breadcrumbs d-flex flex-row align-items-center">
-					<ul>
-					</ul>
-				</div>
-
-				<!-- Sidebar -->
-
-				<div class="sidebar">
-					<div class="sidebar_section">
-						<div class="sidebar_title">
-							<h5>Product Category</h5>
-						</div>
-						<ul class="sidebar_categories">
-							<li ><a href="#">chair</a></li>
-							<li class="active"><a href="#"><span><i class="fa fa-angle-double-right" aria-hidden="true"></i></span>table</a></li>
-							<li><a href="#">shelf</a></li>
-							<li><a href="#">desk</a></li>
-							<li><a href="#">accessory</a></li>
-							<li><a href="#">other</a></li>
-						</ul>
-					</div>
+//--------------------------------------------------------------------------------------
+/*!
+@brief  コマンドが渡されたかどうか
+@return 渡されたらtrue
+*/
+//--------------------------------------------------------------------------------------
+function is_func_active(){
+    if(isset($_POST['func']) && $_POST['func'] != ""){
+        return true;
+    }
+    return false;
+}
 
 
-				</div>
+//--------------------------------------------------------------------------------------
+/*!
+@brief  パラメータのチェック
+@return エラーがあったらfalse
+*/
+//--------------------------------------------------------------------------------------
+function param_chk(){
+     global $ERR_STR;
+    if(!isset($_POST['param']) 
+    || !cutil::is_number($_POST['param'])
+    || $_POST['param'] <= 0){
+        $ERR_STR .= "パラメータを取得できませんでした\n";
+        return false;
+    }
+    return true;
+}
 
-				<!-- Main Content -->
 
-				<div class="main_content">
+//--------------------------------------------------------------------------------------
+/*!
+@brief  データ読み込み
+@return なし
+*/
+//--------------------------------------------------------------------------------------
+function readdata(){
+    global $limit;
+    global $rows;
+    global $page;
+    $obj = new cUSERS();
+    $from = ($page - 1) * $limit;
+    $rows = $obj->get_all(false,$from,$limit);
+}
 
-					<!-- Products -->
+//--------------------------------------------------------------------------------------
+/*!
+@brief  削除
+@return なし
+*/
+//--------------------------------------------------------------------------------------
+function deljob(){
+    $chenge = new cchange_ex();
+    if($_POST['param'] > 0){
+        $chenge->delete("USERS","user_id=" . $_POST['param']);
+    }
+}
 
-					<div class="products_iso">
-						<div class="row">
-							<div class="col">
 
-								<!-- Product Sorting -->
+//--------------------------------------------------------------------------------------
+/*!
+@brief  ページャーのアサイン
+@return なし
+*/
+//--------------------------------------------------------------------------------------
+function assign_page_block(){
+    //$smartyをグローバル宣言（必須）
+    global $smarty;
+    global $limit;
+    global $page;
+    $retstr = '';
+    $obj = new cUSERS();
+    $allcount = $obj->get_all_count(false);
+    $ctl = new cpager($_SERVER['PHP_SELF'],$allcount,$limit);
+    $smarty->assign('pager_arr',$ctl->get('page',$page));
+}
 
-								<div class="product_sorting_container product_sorting_container_top">
-									<ul class="product_sorting">
-										<li>
-											<span class="type_sorting_text">Default Sorting</span>
-											<i class="fa fa-angle-down"></i>
-											<ul class="sorting_type">
-												<li class="type_sorting_btn" data-isotope-option='{ "sortBy": "original-order" }'><span>Default Sorting</span></li>
-												<li class="type_sorting_btn" data-isotope-option='{ "sortBy": "price" }'><span>Price</span></li>
-												<li class="type_sorting_btn" data-isotope-option='{ "sortBy": "name" }'><span>Product Name</span></li>
-											</ul>
-										</li>
-									</ul>
-									<div class="pages d-flex flex-row align-items-center">
-										<div class="page_current">
-											<span>1</span>
-											<ul class="page_selection">
-												<li><a href="#">1</a></li>
-											</ul>
-										</div>
-										<div class="page_total"><span>of</span> 3</div>
-										<div id="next_page" class="page_next"><a href="#"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></div>
-									</div>
 
-								</div>
+//--------------------------------------------------------------------------------------
+/*!
+@brief  一覧のアサイン
+@return なし
+*/
+//--------------------------------------------------------------------------------------
+function assign_admin_master_list(){
+    //$smartyをグローバル宣言（必須）
+    global $smarty;
+    global $rows;
+    $smarty->assign('rows',$rows);
+}
 
-								<!-- Product Grid -->
+//--------------------------------------------------------------------------------------
+/*!
+@brief  URIのアサイン
+@return なし
+*/
+//--------------------------------------------------------------------------------------
+function assign_tgt_uri(){
+    //$smartyをグローバル宣言（必須）
+    global $smarty;
+    global $page;
+    $smarty->assign('tgt_uri',$_SERVER['PHP_SELF'] . '?page=' . $page);
+}
 
-								<div class="product-grid">
 
-									<!-- Product 1 -->
+/////////////////////////////////////////////////////////////////
+/// 関数呼び出しブロック
+/////////////////////////////////////////////////////////////////
+$smarty->assign('ERR_STR',$ERR_STR);
+assign_page_block();
+assign_admin_master_list();
+assign_tgt_uri();
 
-									<div class="product-item men">
-										<div class="product discount product_filter">
-											<div class="product_image">
-												<img src="images/ta1.jpg" alt="" height="200" width="200">
-											</div>
-											<div class="favorite favorite_left"></div>
-											<div class="product_info">
-												<h6 class="product_name"><a href="single.php">すのこテーブル</a></h6>
-												<div class="product_price">4,100円</div>
-											</div>
-											<div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
-									
-</div>
-										</div>
-										
-									<!-- Product 2 -->
+//Smartyを使用した表示(テンプレートファイルの指定)
+$smarty->display('categories.tmpl');
 
-									<div class="product-item women">
-										<div class="product product_filter">
-											<div class="product_image">
-												<img src="images/ta2.jpg" alt="" height="200">
-											</div>
-											<div class="favorite"></div>
-											<div class="product_info">
-												<h6 class="product_name"><a href="single.php">折り畳みアウトドアテーブル</a></h6>
-												<div class="product_price">4,200円</div>
-											</div>
-											<div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
 
-										</div>
-									</div>
-
-									<!-- Product 3 -->
-
-									<div class="product-item women">
-										<div class="product product_filter">
-											<div class="product_image">
-												<img src="images/ta3.jpg" alt="" height="200">
-											</div>
-											<div class="favorite"></div>
-											<div class="product_info">
-												<h6 class="product_name"><a href="single.php">ローテーブル</a></h6>
-												<div class="product_price">3,500円</div>
-											</div>
-											<div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
-
-										</div>
-									</div>
-
-									<!-- Product 4 -->
-
-									<div class="product-item women">
-										<div class="product product_filter">
-											<div class="product_image">
-												<img src="images/table1.jpg" alt="" height="200">
-											</div>
-											<div class="favorite"></div>
-											<div class="product_info">
-												<h6 class="product_name"><a href="single.php">ローテーブル</a></h6>
-												<div class="product_price">5,000円</div>
-											</div>
-											<div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
-
-										</div>
-									</div>
-
-									
-								<!-- Product Sorting -->
-
-								<div class="product_sorting_container product_sorting_container_bottom clearfix">
-
-								</div>
-
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- Footer -->
-
-	<footer class="footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
-
-						<ul class="footer_nav">
-							<li><a href="contact.html">お問い合わせ</a></li>
-							<li><a href="privacy.html">プライバシーポリシー</a></li>
-							<li><a href="kiyaku.html">利用規約</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
-						<ul>
-							<li><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-							<li><a href="https://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-							<li><a href="https://instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="footer_nav_container">
-						<div class="cr">©2019 TeamF imagecraft</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-
-</div>
-
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="styles/bootstrap4/popper.js"></script>
-<script src="styles/bootstrap4/bootstrap.min.js"></script>
-<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
-<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="plugins/easing/easing.js"></script>
-<script src="plugins/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
-<script src="js/categories_custom.js"></script>
-</body>
-
-</html>
+?>
